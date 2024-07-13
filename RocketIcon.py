ebfbaf2868b049b0f5cb32422d1f7c082c02aed6
mao73a <mao73a@o2.pl>
2024-07-13 11:45:26 +0200

    web functions moved to rocketchat_manager class

--- RocketIcon.py
+++ RocketIcon.py
@@ -2,16 +2,13 @@ import pystray
 from PIL import Image
 from pystray import MenuItem as item
 import time
-import requests
+
 import json
 import threading
 import os
 from datetime import datetime, timedelta
-
-
-from rocketchat_async import RocketChat
-import asyncio
-import random
+from  rocketchat_manager import RocketchatManager
+ 
 from icon_manager import icon_manager
 from rules_manager import rules_manager
 
@@ -19,107 +16,44 @@ TITLE = "Better Rocket Icon"
 C_MAIN_LOOP_WAIT_TIME=1 #sec
 
 rules_manager.ensure_local_files()
- 
+pause_invoked = False  # Flag to check if pause_event.clear() was invoked
 stop_event = threading.Event()
 pause_event = threading.Event()  # Event to control pausing
-asyncio_stop_event = None
-asyncio_loop = None
-g_rocketChat = None
-
-pause_invoked = False  # Flag to check if pause_event.clear() was invoked
-
-g_subscription_dict = {}
-g_unread_messages = {}
 subscription_lock = threading.Lock()
+rc_manager = RocketchatManager( subscription_lock, pause_event)
+
 
 # Load configuration from config.json
 def load_config():
-    global config, ROCKET_USER_ID, ROCKET_TOKEN, SERVER_ADDRESS, ROCKET_PROGRAM, TITLE, HEADERS
+    global ROCKET_PROGRAM, TITLE 
     try:
         config = rules_manager.load_config()
-        ROCKET_USER_ID = config['ROCKET_USER_ID']
-        ROCKET_TOKEN = config['ROCKET_TOKEN']
-        SERVER_ADDRESS = config['SERVER_ADDRESS']
         ROCKET_PROGRAM = config['ROCKET_PROGRAM']
-        HEADERS = {
-            'X-Auth-Token': ROCKET_TOKEN,
-            'X-User-Id': ROCKET_USER_ID
-        }
+        with subscription_lock:
+            rc_manager.parse_config(config)
         icon_manager.notify("Config loaded", TITLE)     
+        
     except Exception as e:
         print(f"Error reading config file {e}")
 
-
-
-def convert_to_wsl_address(server_address):
-    if server_address.startswith("https://"):
-        wsl_address = server_address.replace("https://", "wss://") + "/websocket"
-    elif server_address.startswith("http://"):
-        wsl_address = server_address.replace("http://", "ws://") + "/websocket"
-    else:
-        raise ValueError("Invalid server address scheme. Must start with 'http://' or 'https://'.")
-    return wsl_address
-
-
-
-def get_all_subscriptions():
-    try:
-        response = requests.get(f'{SERVER_ADDRESS}/api/v1/subscriptions.get', headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"Failed to fetch data. Status code: {response.status_code}")
-            icon_manager.set_icon_title(f"Failed to fetch data. Status code: {response.status_code}")
-            icon_manager.set_error_image()
-            return None
-    except Exception as e:
-        print(f"Network error: {e}")
-        icon_manager.set_icon_title(f"Network error: {e}")
-        icon_manager.set_error_image()
-        return None
-    
-
-def get_mock_subscriptions():
-    with open('mock_sub.json', 'r') as file:
-        data = json.load(file)
-    return data
-    
-def get_subscription_for_channel(channel_id):
-    return get_mock_subscriptions()
-    try:
-        response = requests.get(f'{SERVER_ADDRESS}/api/v1/subscriptions.getOne?roomId={channel_id}', headers=HEADERS)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"Failed to fetch data. Status code: {response.status_code}")
-            set_icon_title(f"Failed to fetch data. Status code: {response.status_code}")
-            icon_manager.set_error_image()
-            return None
-    except Exception as e:
-        print(f"Network error: {e}")
-        set_icon_title(f"Network error: {e}")
-        icon_manager.set_error_image()
-        return None    
-    
 def get_channels_for_messages(channels):
     updates = []
     for channel_id in channels:
-          json = get_subscription_for_channel(channel_id)
+          json = rc_manager.get_subscription_for_channel(channel_id)
           updates.append(json.get('subscription'))
     return {"update":updates}
     
+def check_config_loaded():
+    if not rules_manager.rules_are_loaded():
+        my_on_error("Error reading rules.json file. Please go to Rules and verify your JSON syntax, and try again.")
+        stop_event.wait(10)
+        return False 
 
-def get_last_message_text(rid):
-    if rid not in g_unread_messages or not g_unread_messages[rid]:
-        return None
-    last_message = g_unread_messages[rid][-1]
-    last_msg_content = next(iter(last_message.values()))
-    if last_msg_content.get("qualifier") == "videoconf":
-        txt = "Incoming phone call"
-    else:
-        txt = last_msg_content.get("text")
-    return txt
-
+    if not rules_manager.config_is_loaded():
+        my_on_error("Error reading config.json file. Please go to Settings and verify your JSON syntax, and try again.")
+        stop_event.wait(10)
+        return False
+    return True             
 
 def monitor_all_subscriptions():
     time.sleep(1)
@@ -127,26 +61,16 @@ def monitor_all_subscriptions():
     try:
         while not stop_event.is_set():
             pause_event.wait()  # Wait for the pause event to be set
-
-            if not rules_manager.rules_are_loaded():
-                icon_manager.set_error_image()
-                icon_manager.set_icon_title("Error reading rules.json file. Please go to Rules and verify your JSON syntax, and try again.")
-                stop_event.wait(10)
+            if not check_config_loaded():
                 continue
 
-            if not rules_manager.config_is_loaded():
-                icon_manager.set_error_image()
-                icon_manager.set_icon_title("Error reading config.json file. Please go to Settings and verify your JSON syntax, and try again.")
-                stop_event.wait(10)
-                continue          
-
             with subscription_lock:      
-                data = get_channels_for_messages(g_unread_messages)
+                data = get_channels_for_messages(rc_manager.unread_messages)
                 if data:
                     updates = data.get('update', [])
                     icon_manager.reset_priority()
                     for sub in updates:
-                        rules_manager.process_subscription(sub, g_unread_messages)                
+                        rules_manager.process_subscription(sub, rc_manager.unread_messages)                
                     if rules_manager.unread_counts:
                         summary = "\n".join([f"{fname}: {unread}" for fname, unread in rules_manager.unread_counts.items()])
                     else:
@@ -158,100 +82,16 @@ def monitor_all_subscriptions():
                     icon_manager.set_basic_image()
             stop_event.wait(C_MAIN_LOOP_WAIT_TIME)
     finally:
-        stop_asyncio_subscriptions_websocket()
-
-
-
-#{'motest':['msg_101':{"text":"abc"}, 'msg_102':{"text":"efg",  "qualifier":"videoconf"} ]}
-
-def remove_message(channel_id, msg_id):
-     return
-     for msg_dict in g_unread_messages[channel_id]:
-        if msg_id in msg_dict:
-            g_unread_messages[channel_id].remove(msg_dict)
-            break
-
-def handle_message(channel_id, sender_id, msg_id, thread_id, msg, qualifier, unread, repeated):
-    with subscription_lock:
-        print(f"channel={channel_id} sender={sender_id} msgid={msg_id} txt={msg}  unread={unread} qualifier={qualifier}")
-        #qualifier=
-        if not g_unread_messages.get(channel_id):
-            if not unread:
-                return
-            else:
-                g_unread_messages[channel_id] = []
-        if unread:
-            g_unread_messages[channel_id].append({msg_id: {"text": msg, "qualifier":qualifier}})
-            print("   -- handle_message1 dodano ")
-        else:
-            remove_message(channel_id, msg_id)
-            if len(g_unread_messages[channel_id])==0:
-                del g_unread_messages[channel_id]
-            print("   -- handle_message1 usunieto ")
-
-#asyncio.run(monitor_subscriptions_websocket()
-async def monitor_subscriptions_websocket():
-    global asyncio_stop_event
-    global g_rocketChat
-    icon_manager.set_basic_image()
-    asyncio_stop_event = asyncio.Event()
-    while not asyncio_stop_event.is_set():
-        if not rules_manager.rules_are_loaded() or not rules_manager.config_is_loaded():
-            icon_manager.set_error_image()
-            await asyncio.sleep(10)
-            continue
-
-        print("#4")
-        pause_event.wait()  # Wait for the pause event to be set
-        try:
-            print("#5")
-            g_rocketChat = RocketChat()
-            #await rc.start(address, username, password)
-            # Alternatively, use rc.resume for token-based authentication:
-            await g_rocketChat.resume(convert_to_wsl_address(SERVER_ADDRESS), ROCKET_USER_ID, ROCKET_TOKEN)
-            print("#6")
-            # 1. Set up the desired callbacks...
-            #for channel_id, channel_type in await rc.get_channels(): //to few informations returned :-(
-            data = get_all_subscriptions()
-            print("#7")
-            if data:
-                with subscription_lock:
-                    updates = data.get('update', [])
-                    for sub in updates:
-                        fname = sub.get('fname')
-                        chtype = sub.get('t')
-                        channel_id = sub.get('rid')
-                        open = sub.get('open')
-                        if open==True:
-                            matching_rule = rules_manager.find_matching_rule(fname, chtype, [])
-                            if matching_rule and matching_rule.get('ignore', "False") == "False":
-                                g_subscription_dict[channel_id]=sub
-                                await g_rocketChat.subscribe_to_channel_messages(channel_id,  handle_message)
-                                print(f'subscribed to  {fname}  {channel_id}')
-                # 2. ...and then simply wait for the registered events.
-                await g_rocketChat.run_forever()
-        except  (RocketChat.ConnectionClosed, RocketChat.ConnectCallFailed) as e:
-                icon_manager.set_icon_title(f"Connection failed: {e}")
-                icon_manager.set_error_image()
-                print(f'Connection failed: {e}. Waiting a few seconds...')
-                await asyncio.sleep(random.uniform(4, 8))
-                print('Reconnecting...')
-
-
-
-
-
+        rc_manager.stop()
 
 
 def on_clicked_quit(icon, item):
     if item.text == "Quit":
         print("Quit.")        
-        stop_asyncio_subscriptions_websocket()
+        rc_manager.stop()
         stop_event.set()
         pause_event.set()  # Resume if paused to ensure clean exit
-        time.sleep(5)        
         icon.stop()
-
         print("Normal exit.")
 
 def on_clicked_show(icon, item):
@@ -306,7 +146,7 @@ def on_clicked_separator(icon, item):
     return
 
 def on_clicked_subscriptions(icon, item):
-    json_data = json.dumps(get_all_subscriptions(), indent=4, sort_keys=True)
+    json_data = json.dumps(rc_manager.get_all_subscriptions(), indent=4, sort_keys=True)
     file_path = "subscriptions.txt"
     with open(file_path, 'w') as file:
         file.write(json_data)
@@ -329,30 +169,12 @@ def setup(icon):
         pystray.MenuItem("Quit", on_clicked_quit)
     )
 
-def monitor_asyncio_subscriptions_websocket():
-    global asyncio_loop
-    asyncio_loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(asyncio_loop)
-    print("#2")
-    try:
-        asyncio_loop.run_until_complete(monitor_subscriptions_websocket())
-    except Exception as e:
-        print(f"Exception: {e}")
-    finally:
-        print("Finally")
-        if g_rocketChat:
-            g_rocketChat.cleanup_pending_task(asyncio_loop)
-        print("Finally end")    
-
-def stop_asyncio_subscriptions_websocket():
-    global asyncio_loop
-    asyncio_stop_event.set()
-    if g_rocketChat:
-        g_rocketChat.stop()
-    # g_rocketChat.cancel_task(asyncio_loop)
+def my_on_error(text):
+    icon_manager.set_icon_title(text)
+    icon_manager.set_error_image()     
 
 def my_on_escalation(channel):
-     icon_manager.notify(f"You have still unread messages from {channel}", "Unread messages")
+    icon_manager.notify(f"You have still unread messages from {channel}", "Unread messages")
 
 def my_on_file_changed(filename):
     if filename=='config.json':
@@ -367,7 +189,7 @@ def my_on_unread_message(matching_rule, subscription, is_new_message):
     if is_new_message:
         icon_manager.play_sound(matching_rule.get("sound", rules_manager.DEFAULTS.get("sound")))
         if (matching_rule.get("preview", rules_manager.DEFAULTS.get("preview"))) == "True":
-            icon_manager.notify(f"{get_last_message_text(rid)}", fname)        
+            icon_manager.notify(f"{rc_manager.get_last_message_text(rid)}", fname)        
 
 
 
@@ -379,8 +201,9 @@ if __name__ == "__main__":
     rules_manager.set_on_unread_message(my_on_unread_message)
     pause_event.set()  # Ensure the pause event is initially set to allow monitoring
     threading.Thread(target=monitor_all_subscriptions).start()
-       
-    threading.Thread(target=monitor_asyncio_subscriptions_websocket).start()
+    rc_manager.set_on_error_callback(my_on_error)
+    rc_manager.start() # start a new thread
+
     time.sleep(1)
     # for sig in (signal.SIGINT, signal.SIGTERM):
     #     asyncio_loop.add_signal_handler(sig, stop_loop)
---
+++ rocketchat_manager.py
@@ -0,0 +1,191 @@
+import asyncio
+import threading
+from rules_manager import rules_manager
+from rocketchat_async import RocketChat
+import requests
+import json
+
+class RocketchatManager:
+    def __init__(self, subscription_lock, pause_event):
+        self.ROCKET_USER_ID = ''
+        self.ROCKET_TOKEN = ''
+        self.SERVER_ADDRESS = ''
+        self.HEADERS = {}
+        self._on_error_callback = None
+        self._subscription_lock = subscription_lock
+        self._pause_event = pause_event
+        self._stop_event = None
+        self.unread_messages = {}
+        self._rocketChat = None
+        self._subscription_dict = {}
+
+
+    def set_on_error_callback(self, callback):
+        self._on_error_callback = callback
+
+
+    def parse_config(self, config):
+        self.ROCKET_USER_ID = config['ROCKET_USER_ID']
+        self.ROCKET_TOKEN = config['ROCKET_TOKEN']
+        self.SERVER_ADDRESS = config['SERVER_ADDRESS']
+        self.HEADERS = {
+            'X-Auth-Token': self.ROCKET_TOKEN,
+            'X-User-Id': self.ROCKET_USER_ID
+        }
+ 
+
+
+    def get_mock_subscriptions(self):
+        with open('mock_sub.json', 'r') as file:
+            data = json.load(file)
+        return data
+        
+    def get_subscription_for_channel(self,channel_id):
+        return self.get_mock_subscriptions()
+        try:
+            response = requests.get(f'{self.SERVER_ADDRESS}/api/v1/subscriptions.getOne?roomId={channel_id}', headers=self.HEADERS)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                self.do_error(f"Failed to fetch data. Status code: {response.status_code}")
+                return None
+        except Exception as e:
+            self.do_error(f"Network error: {e}")
+            return None             
+
+    def get_all_subscriptions(self):
+        try:
+            response = requests.get(f'{self.SERVER_ADDRESS}/api/v1/subscriptions.get', headers= self.HEADERS)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                self.do_error(f"Failed to fetch data. Status code: {response.status_code}")
+                return None
+        except Exception as e:
+            self.do_error(f"Network error: {e}")        
+            return None          
+
+    def convert_to_wsl_address(self, server_address):
+        if server_address.startswith("https://"):
+            wsl_address = server_address.replace("https://", "wss://") + "/websocket"
+        elif server_address.startswith("http://"):
+            wsl_address = server_address.replace("http://", "ws://") + "/websocket"
+        else:
+            raise ValueError("Invalid server address scheme. Must start with 'http://' or 'https://'.")
+        return wsl_address    
+
+    def do_error(self, text):
+        print(text)        
+        if self._on_error_callback:
+            self._on_error_callback(text)
+
+    def get_last_message_text(self, rid):
+        if rid not in self.unread_messages or not self.unread_messages[rid]:
+            return None
+        last_message = self.unread_messages[rid][-1]
+        last_msg_content = next(iter(last_message.values()))
+        if last_msg_content.get("qualifier") == "videoconf":
+            txt = "Incoming phone call"
+        else:
+            txt = last_msg_content.get("text")
+        return txt
+
+    #{'motest':['msg_101':{"text":"abc"}, 'msg_102':{"text":"efg",  "qualifier":"videoconf"} ]}
+
+    def remove_message(self, channel_id, msg_id):
+        return
+        for msg_dict in g_unread_messages[channel_id]:
+            if msg_id in msg_dict:
+                g_unread_messages[channel_id].remove(msg_dict)
+                break
+
+    def handle_message(self, channel_id, sender_id, msg_id, thread_id, msg, qualifier, unread, repeated):
+        with self._subscription_lock:
+            print(f"channel={channel_id} sender={sender_id} msgid={msg_id} txt={msg}  unread={unread} qualifier={qualifier}")
+            #qualifier=
+            if not self.unread_messages.get(channel_id):
+                if not unread:
+                    return
+                else:
+                    self.unread_messages[channel_id] = []
+            if unread:
+                self.unread_messages[channel_id].append({msg_id: {"text": msg, "qualifier":qualifier}})
+                print("   -- handle_message1 dodano ")
+            else:
+                self.remove_message(channel_id, msg_id)
+                if len(self.unread_messages[channel_id])==0:
+                    del self.unread_messages[channel_id]
+                print("   -- handle_message1 usunieto ")
+
+    #asyncio.run(monitor_subscriptions_websocket()
+    async def monitor_subscriptions_websocket(self):
+        self._stop_event = asyncio.Event()        
+        while not self._stop_event.is_set():
+            if not rules_manager.rules_are_loaded() or not rules_manager.config_is_loaded():
+                self.do_error('')
+                await asyncio.sleep(10)
+                continue
+
+            print("#4")
+            self._pause_event.wait()  # Wait for the pause event to be set
+            try:
+                print("#5")
+                self._rocketChat = RocketChat()
+                #await rc.start(address, username, password)
+                # Alternatively, use rc.resume for token-based authentication:
+                await self._rocketChat.resume(self.convert_to_wsl_address(self.SERVER_ADDRESS), self.ROCKET_USER_ID, self.ROCKET_TOKEN)
+                print("#6")
+                # 1. Set up the desired callbacks...
+                #for channel_id, channel_type in await rc.get_channels(): //to few informations returned :-(
+                data = self.get_all_subscriptions()
+                print("#7")
+                if data:
+                    with self._subscription_lock:
+                        updates = data.get('update', [])
+                        for sub in updates:
+                            fname = sub.get('fname')
+                            chtype = sub.get('t')
+                            channel_id = sub.get('rid')
+                            open = sub.get('open')
+                            if open==True:
+                                matching_rule = rules_manager.find_matching_rule(fname, chtype, [])
+                                if matching_rule and matching_rule.get('ignore', "False") == "False":
+                                    self._subscription_dict[channel_id]=sub
+                                    await self._rocketChat.subscribe_to_channel_messages(channel_id,  self.handle_message)
+                                    print(f'subscribed to  {fname}  {channel_id}')
+                    # 2. ...and then simply wait for the registered events.
+                    await self._rocketChat.run_forever()
+                else:
+                    self.do_error("No subscirptions found")
+                    await asyncio.sleep(10)
+                    continue                        
+            except  (RocketChat.ConnectionClosed, RocketChat.ConnectCallFailed) as e:
+                    self.do_error("Connection failed: {e}")
+                    await asyncio.sleep(10)
+                    print('Reconnecting...')
+
+
+    def monitor_asyncio_subscriptions_websocket(self):
+        asyncio_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(asyncio_loop)
+        try:
+            asyncio_loop.run_until_complete(self.monitor_subscriptions_websocket())
+        except Exception as e:
+            print(f"Exception: {e}")
+        finally:
+            print("Finally...")
+            if self._rocketChat:
+                self._rocketChat.cleanup_pending_task(asyncio_loop)
+            print("monitor_asyncio_subscriptions_websocket end")    
+
+    def stop(self):
+        self._stop_event.set()
+        if self._rocketChat:
+            self._rocketChat.stop()
+
+    def start(self):
+        threading.Thread(target=self.monitor_asyncio_subscriptions_websocket).start()
+
+
+
+
