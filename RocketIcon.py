f99fe3a52802d6d9311a26f5d44c3ea91e6b445e
Marek Ochal <marek.ochal@comarch.pl>
2024-07-23 15:26:42 +0200

    Added AUTOAWAY_TIME_SEC

--- RocketIcon.py
+++ RocketIcon.py
@@ -2,17 +2,17 @@ import pystray
 from pystray import  Menu,  MenuItem as item
 import time
 import shutil
-import json
+import sys
 import threading
 import os
 from datetime import datetime, timedelta
 from RocketIcon import RocketchatManager, icon_manager, RulesManager
 from RocketIcon.proxy_server import run_proxy_server, get_proxy_url
 import requests
- 
+
 TITLE = "Rocket Icon"
 C_MAIN_LOOP_WAIT_TIME=1 #sec
-       
+
 pause_invoked = False  # Flag to check if pause_event.clear() was invoked
 stop_event = threading.Event()
 pause_event = threading.Event()
@@ -27,7 +27,7 @@ def ensure_local_files():
     if not os.path.exists(os.path.join(config_path, 'config.json')):
         try:
             os.makedirs(config_path)
-        finally:            
+        finally:
             shutil.copy('config.json', os.path.join(config_path, 'config.json'))
             shutil.copy('rules.json', os.path.join(config_path, 'rules.json'))
 
@@ -36,16 +36,17 @@ ensure_local_files()
 
 # Load configuration from config.json
 def load_config():
-    global ROCKET_PROGRAM, TITLE 
+    global ROCKET_PROGRAM, TITLE, AUTOAWAY_TIME_SEC
     try:
         config = rules_manager.load_config()
         ROCKET_PROGRAM = config['ROCKET_PROGRAM']
+        AUTOAWAY_TIME_SEC = config.get('AUTOAWAY_TIME_SEC', 5*60)
         with subscription_lock:
             rc_manager.set_ROCKET_USER_ID(config['ROCKET_USER_ID'])
             rc_manager.set_ROCKET_TOKEN(config['ROCKET_TOKEN'])
             rc_manager.set_SERVER_ADDRESS(config['SERVER_ADDRESS'])
-        icon_manager.notify("Config loaded", TITLE)     
-        
+        icon_manager.notify("Config loaded", TITLE)
+
     except Exception as e:
         print(f"Error reading config file {e}")
 
@@ -55,41 +56,64 @@ def get_channels_for_messages(channels):
           json = rc_manager.get_subscription_for_channel(channel_id)
           updates.append(json.get('subscription'))
     return {"update":updates}
-    
+
 def check_config_loaded():
     if not rules_manager.rules_are_loaded():
         my_on_error("Error reading rules.json file. Please go to Rules and verify your JSON syntax, and try again.")
         stop_event.wait(10)
-        return False 
+        return False
 
     if not rules_manager.config_is_loaded():
         my_on_error("Error reading config.json file. Please go to Settings and verify your JSON syntax, and try again.")
         stop_event.wait(10)
         return False
-    return True             
+    return True
+
+if sys.platform == 'win32':
+    from ctypes import *
+
+    class LASTINPUTINFO(Structure):
+        _fields_ = [
+            ('cbSize', c_uint),
+            ('dwTime', c_int),
+        ]
+
+    def get_idle_duration():
+        lastInputInfo = LASTINPUTINFO()
+        lastInputInfo.cbSize = sizeof(lastInputInfo)
+        if windll.user32.GetLastInputInfo(byref(lastInputInfo)):
+            millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+            return millis / 1000.0
+        else:
+            return 0
+else:
+    def get_idle_duration():
+        return 0
+
 
 def monitor_all_subscriptions():
     time.sleep(1)
     print(f"Starting loop")
     counter = 1
     prev_status="Unknown"
-    previous_time = time.time()    
+    need_restore_status=""
+    previous_time = time.time()
     try:
         while not stop_event.is_set():
             pause_event.wait()  # Wait for the pause event to be set
             if not check_config_loaded():
                 continue
 
-            with subscription_lock:   
+            with subscription_lock:
                 current_time = time.time()
-                elapsed_time = current_time - previous_time 
-                
+                elapsed_time = current_time - previous_time
+
                 data = get_channels_for_messages(rc_manager.unread_messages)
                 if data:
                     updates = data.get('update', [])
                     icon_manager.reset_priority()
                     for sub in updates:
-                        rules_manager.process_subscription(sub, rc_manager.unread_messages)                
+                        rules_manager.process_subscription(sub, rc_manager.unread_messages)
                     if rules_manager.unread_counts:
                         summary = "\n".join([f"{fname}: {unread}" for fname, unread in rules_manager.unread_counts.items()])
                     else:
@@ -102,19 +126,27 @@ def monitor_all_subscriptions():
 
                 if counter % 5 == 0:
                     status = rc_manager.get_status()
+                    idle_duration_sec = get_idle_duration()
                     if status!=prev_status:
                         icon_manager.icon.update_menu()
                         prev_status=status
-                        print(f"User status is now {status}")
- 
+                        print(f"User status is now {status}. AutoAway set to {AUTOAWAY_TIME_SEC}")
+                    if status=='online' and idle_duration_sec>AUTOAWAY_TIME_SEC:
+                        rc_manager.set_away('')
+                        need_restore_status=status
+                    elif need_restore_status and idle_duration_sec<=AUTOAWAY_TIME_SEC:
+                        rc_manager.set_user_status(need_restore_status)
+                        need_restore_status=""
+
+
             if elapsed_time > 5:
                 print("Reinitialize connections after wakeup...")
                 if rc_manager.get_status()=='away':
                     rc_manager.set_online()
                 restart() #restart after sleep
 
-            counter += 1    
-            previous_time = time.time() 
+            counter += 1
+            previous_time = time.time()
             stop_event.wait(C_MAIN_LOOP_WAIT_TIME)
     except Exception as e:
         print(f"Fatal error {e}")
@@ -129,13 +161,13 @@ def quit():
     icon_manager.stop()
     if proxy_thread:
         requests.get(f"{get_proxy_url()}/shutdown")
-        proxy_thread.join()        
+        proxy_thread.join()
 
 
 
 def on_clicked_quit(icon, item):
     if item.text == "Quit":
-        print("Quit.")   
+        print("Quit.")
         quit()
         print("Normal exit.")
 
@@ -145,7 +177,7 @@ def on_clicked_show(icon, item):
         icon_manager.set_delay_image()
     else:
         icon_manager.set_launch_image()
-    launch_program = ROCKET_PROGRAM    
+    launch_program = ROCKET_PROGRAM
     if "{ROOM}" in launch_program:
         launch_program = launch_program.replace("{ROOM}", rules_manager.get_room_to_visit(), 1)
     os.startfile(launch_program)
@@ -213,59 +245,59 @@ def on_clicked_busy(icon, item):
 
 
 def on_clicked_away(icon, item):
-    rc_manager.set_away('')    
+    rc_manager.set_away('')
 
 
 def on_clicked_offline(icon, item):
-    rc_manager.set_offline()       
+    rc_manager.set_offline()
 
-    
+
 def restart():
-    pause_event.clear() 
-    rules_manager.reset()    
+    pause_event.clear()
+    rules_manager.reset()
     rc_manager.restart()
     rc_manager.mark_read()
     pause_event.set()
 
 def on_mark_read():
-    rc_manager.mark_read()    
+    rc_manager.mark_read()
     restart()
- 
+
 
 def on_version(icon, item):
      os.startfile(f"https://github.com/mao73a/rocket.icon/releases")
- 
+
 def setup(icon):
     icon.visible = True
     icon.menu = pystray.Menu(
-  
+
         pystray.MenuItem("Version 1.0.3", on_version),
         pystray.MenuItem("Settings", on_clicked_settings),
         pystray.MenuItem("Rules", on_clicked_rules),
-        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),     
-        pystray.MenuItem("Search", on_search),             
-        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),  
-        pystray.MenuItem("Pause...",  
+        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),
+        pystray.MenuItem("Search", on_search),
+        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),
+        pystray.MenuItem("Pause...",
                 Menu(pystray.MenuItem("Pause for 10 minutes", on_clicked_stop_10),
                      pystray.MenuItem("Pause for 25 minutes", on_clicked_stop_25),
                      pystray.MenuItem("Pause for 60 minutes", on_clicked_stop_60),
                      pystray.MenuItem("Pause for 120 minutes",on_clicked_stop_120))),
         pystray.MenuItem("Resume", on_clicked_resume),
-        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),          
+        pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),
         pystray.MenuItem('Online',  on_clicked_online, checked=lambda item: rc_manager.get_status()=="online", radio=True),
         pystray.MenuItem('Busy',    on_clicked_busy, checked=lambda item: rc_manager.get_status()=="busy", radio=True),
         pystray.MenuItem('Away',    on_clicked_away, checked=lambda item: rc_manager.get_status()=="away", radio=True),
-        pystray.MenuItem('Offline', on_clicked_offline, checked=lambda item: rc_manager.get_status()=="offline", radio=True),             
+        pystray.MenuItem('Offline', on_clicked_offline, checked=lambda item: rc_manager.get_status()=="offline", radio=True),
         pystray.MenuItem(pystray.Menu.SEPARATOR, on_clicked_separator),
-        pystray.MenuItem("Launch Rocket", on_clicked_show, default=True),       
-        pystray.MenuItem("Mark all as read", on_mark_read),                 
+        pystray.MenuItem("Launch Rocket", on_clicked_show, default=True),
+        pystray.MenuItem("Mark all as read", on_mark_read),
         pystray.MenuItem("Quit", on_clicked_quit)
     )
 
 
 def my_on_error(text):
     icon_manager.set_icon_title(text)
-    icon_manager.set_error_image()     
+    icon_manager.set_error_image()
 
 def my_on_escalation(channel):
     icon_manager.notify(f"You have still unread messages from {channel}", "Unread messages")
@@ -274,27 +306,27 @@ def my_on_file_changed(filename):
     if filename=='config.json':
         load_config()
     else:
-        icon_manager.notify(f"Rules reloaded", "Rules")  
+        icon_manager.notify(f"Rules reloaded", "Rules")
 
 def my_on_reload():
     icon_manager.set_reload_image()
 
 def my_on_unread_message(matching_rule, subscription, is_new_message):
     fname = subscription.get('fname')
-    rid = subscription.get('rid')    
+    rid = subscription.get('rid')
     icon_manager.set_notification_image(matching_rule.get("icon", rules_manager.DEFAULTS.get("icon")), matching_rule.get("prior"),
                                             matching_rule.get("blink_delay", rules_manager.DEFAULTS.get("blink_delay", 0)))
     if is_new_message:
         icon_manager.play_sound(matching_rule.get("sound", rules_manager.DEFAULTS.get("sound")))
         if (matching_rule.get("preview", rules_manager.DEFAULTS.get("preview"))) == "True":
-            icon_manager.notify(f"{rc_manager.get_last_message_text(rid)}", fname)      
+            icon_manager.notify(f"{rc_manager.get_last_message_text(rid)}", fname)
 
 
 if __name__ == "__main__":
     icon_manager.set_icon_title(TITLE)
     load_config()
     rules_manager.set_on_escalation(my_on_escalation)
-    rules_manager.set_on_file_changed(my_on_file_changed, stop_event)  # starts a new thread   
+    rules_manager.set_on_file_changed(my_on_file_changed, stop_event)  # starts a new thread
     rules_manager.set_on_unread_message(my_on_unread_message)
     pause_event.set()  # Ensure the pause event is initially set to allow monitoring
     threading.Thread(target=monitor_all_subscriptions).start()
@@ -307,7 +339,7 @@ if __name__ == "__main__":
     # Start the proxy server in a separate thread
     proxy_thread = threading.Thread(target=run_proxy_server, args=(rc_manager,rules_manager, ))
     proxy_thread.start()
-  
+
     time.sleep(1)
     # for sig in (signal.SIGINT, signal.SIGTERM):
     #     asyncio_loop.add_signal_handler(sig, stop_loop)
